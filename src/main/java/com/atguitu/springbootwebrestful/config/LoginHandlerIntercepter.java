0ae9e24067e08a2ba81171096466581d1dda3664
huahua <huahuazhang1988@gmail.com>
2022-12-20 13:14:09 +0100

    "learning restful"

---
+++ src/main/java/com/atguitu/springbootwebrestful/config/LoginHandlerIntercepter.java
@@ -0,0 +1,32 @@
+package com.atguitu.springbootwebrestful.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class LoginHandlerIntercepter implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HttpSession session = request.getSession();
+        Object user = session.getAttribute("loginUser");
+        if(user!=null){
+            request.getRequestDispatcher("/dashboard.html").forward(request,response);
+            return true;
+
+        }else {
+            return false;
+        }
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+}
